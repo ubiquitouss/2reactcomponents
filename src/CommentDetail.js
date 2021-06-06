19bd29f6a986f20455325a2e0f0b4aea57503b82
ubiquitouss <ubiquitouss@yahoo.com>
2021-06-06 16:34:13 +0600

    props image also working

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import faker from 'faker'
 
 //first argument can be anything and that will be used as props
 const CommentDetail = (props) => {
@@ -7,17 +6,17 @@ const CommentDetail = (props) => {
     return (
         <div className="comment">
             <a href="/" className="avatar">
-            <img alt="avatar" src={faker.image.avatar()}/>
+            <img alt="avatar" src={props.avatar}/>
             </a>
             <div className="content">
                 <a href="/" className="author">
                     {props.author}
                 </a>
                 <div className="metadata">
-                    <span className="date">Today at 6:00 pm</span>
+                    <span className="date">{props.timeAgo}</span>
                 </div>
                 <div className="text">
-                    Nice blog post
+                    {props.blogPost}
                 </div>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -9,9 +9,21 @@ import faker from 'faker';
 const App =()=>{
     return(
        <div className="ui container comments">
-           <CommentDetail author="Sam"/>
-           <CommentDetail author="Alex"/>
-           <CommentDetail author="Jane"/>
+           <CommentDetail 
+           author="Sam" 
+           timeAgo="Today at 4:45PM" 
+           blogPost="this is amazing"
+           avatar={faker.image.avatar()} />
+           <CommentDetail 
+           author="Alex" 
+           timeAgo="Today at 2:00PM"
+           blogPost="I've some other things"
+           avatar={faker.image.avatar()}/>
+           <CommentDetail 
+           author="Jane" 
+           timeAgo="Yesterday at 5:23AM" 
+           blogPost="I really have some other things"
+           avatar={faker.image.avatar()}/>
         </div>
        
     );
